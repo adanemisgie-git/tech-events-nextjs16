803c45c63b62123ff171363b562581579becd43f
adanemisgie-git <adanemisgie01@gmail.com>
2026-01-09 17:43:39 +0300

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,68 @@
+
+
+export const events = [
+  {
+    title: "React Summit 2026",
+    image: "/images/event1.png",
+    slug: "react-summit-2026",
+    location: "Amsterdam, Netherlands",
+    date: "2026-06-10",
+    time: "09:00 AM - 6:00 PM",
+  },
+  {
+    title: "JSConf EU",
+    image: "/images/event2.png",
+    slug: "jsconf-eu-2026",
+    location: "Berlin, Germany",
+    date: "2026-09-15",
+    time: "10:00 AM - 5:00 PM",
+  },
+  {
+    title: "HackMIT",
+    image: "/images/event3.png",
+    slug: "hackmit-2026",
+    location: "Cambridge, MA, USA",
+    date: "2026-04-20",
+    time: "08:00 AM - 8:00 PM",
+  },
+  {
+    title: "PyCon US",
+    image: "/images/event4.png",
+    slug: "pycon-us-2026",
+    location: "Salt Lake City, UT, USA",
+    date: "2026-05-01",
+    time: "09:00 AM - 6:00 PM",
+  },
+  {
+    title: "DevFest London",
+    image: "/images/event5.png",
+    slug: "devfest-london-2026",
+    location: "London, UK",
+    date: "2026-07-22",
+    time: "10:00 AM - 7:00 PM",
+  },
+  {
+    title: "ETHGlobal Hackathon",
+    image: "/images/event6.png",
+    slug: "ethglobal-hackathon-2026",
+    location: "Online",
+    date: "2026-03-12",
+    time: "All Day",
+  },
+  {
+    title: "Google I/O",
+    image: "/images/event2.png",
+    slug: "google-io-2026",
+    location: "Mountain View, CA, USA",
+    date: "2026-05-15",
+    time: "09:00 AM - 5:00 PM",
+  },
+  {
+    title: "Next.js Conf",
+    image: "/images/event-full.png",
+    slug: "nextjs-conf-2026",
+    location: "San Francisco, CA, USA",
+    date: "2026-08-18",
+    time: "10:00 AM - 6:00 PM",
+  }
+];
